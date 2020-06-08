9ef5829ddc4283b991fdb93c6c835733648cc102
Renato Vieira <vierrra@hotmail.com>
2020-06-07 23:41:03 -0300

    Changed class main

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from googletrans import Translator
 message = AnaliseSentimentos()
 
 translatedText = Translator().translate(text=input('Digite um texto: '), dest='pt')
-result         = message.avalia(translatedText.text)
+message.avalia(translatedText.text)
 
 
